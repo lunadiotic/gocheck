376aa3923786116e8179faa2cebabe18b13b5538
aimeliala <idstack@outlook.co.id>
2023-07-18 00:34:14 +0700

    contoh kondisi deriving state

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={listItems} />
     </div>
   );
 }
@@ -113,10 +113,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (items.length === 0) {
+    return (
+      <footer className="stats">
+        <span>📝 Yuk mulai bikin catatan!</span>
+      </footer>
+    );
+  }
+
+  const totalItems = items.length;
+  const doneItems = items.filter((item) => item.done).length;
+  const percentage = Math.round((doneItems / totalItems) * 100);
   return (
     <footer className="stats">
-      <span>🗒️ Kamu punya x catatan dan baru x yg dichecklist (x%) ✅</span>
+      <span>
+        {percentage === 100
+          ? "✅ Kamu sudah melakukannya semua"
+          : `🗒️ Kamu punya ${totalItems} catatan dan baru ${doneItems} yg dichecklist (${percentage}%)`}
+      </span>
     </footer>
   );
 }
